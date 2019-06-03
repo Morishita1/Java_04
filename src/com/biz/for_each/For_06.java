b88332b2d04dd4ef96a6b491529a82adfbdfa45b
Morishtia1 <iakwpi@naver.com>
2019-06-03 09:56:30 +0900

    first

---
+++ src/com/biz/for_each/For_06.java
@@ -0,0 +1,73 @@
+package com.biz.for_each;
+
+public class For_06 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 여러줄의 주석
+		 * 
+		 * intMin 부터, inMax까지의
+		 * 1. 전체합
+		 * 2. 짝수의 합
+		 * 3. 홀수의 합
+		 */
+		
+		
+		
+		int intMin =0;
+		int intMax =10;
+		int intSum =0;
+		System.out.println(intMin+"부터 "+intMax+"까지의 전체 합");
+		for(intMin=1;intMin<=intMax;intMin++) {
+			intSum=intSum+intMin;
+		}
+		System.out.println(intSum);
+		
+		intSum=0;
+		intMin=0;
+		
+		System.out.println(intMin+"부터 "+intMax+"까지의 짝수의 합");
+		for(intMin=2;intMin<=intMax;intMin+=2) {
+			intSum=intSum+intMin;
+		}
+		System.out.println(intSum);
+		
+		intSum=0;
+		intMin=0;
+		
+		System.out.println(intMin+"부터 "+intMax+"까지의 홀수의 합");
+		for(intMin=1;intMin<=intMax;intMin+=2) {
+			intSum=intSum+intMin;
+		}
+		System.out.println(intSum);
+		
+		intSum=0;
+		intMin=0;
+		
+	
+		int intEvenSum=0;
+		int intOddSum=0;
+		
+		for(int i= intMin;i<=intMax;i++) {
+			intSum +=i;
+			
+		}
+		// 짝수의 시작값 계산
+		int intEven=intMin+intMin%2;
+		for(int i=intEven; i<=intMax;i+=2) {
+			intEvenSum+=i;
+			
+		}
+		
+		// 홀수의 시작값 계산
+		int intOdd=intMin+(intMin+1)%2;
+		for(int i=intOdd;i<=intMax;i+=2) {
+			intOddSum+=i;
+		}
+		System.out.println("전체합: "+ intSum);
+		System.out.println("짝수합: "+ intEvenSum);
+		System.out.println("홀수합: "+ intOddSum);
+	}
+
+}
---
+++ src/com/biz/if_then/If_06.java
@@ -0,0 +1,22 @@
+package com.biz.if_then;
+
+public class If_06 {
+
+	public static void main(String[] args) {
+
+		int i=7;
+		int index =2;
+		for(index=2;index<i;index++) {
+			if(i%index==0) {
+				//System.out.println(i+"는 소수가 아님");
+				break;
+			}
+		}
+		if(index>=i) {
+			System.out.println(index+"는 소수입니다");
+		}else {
+			System.out.println(i+"는 소수가 아님");
+		}
+	}
+
+}
